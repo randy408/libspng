b70f7beead818c6f2e5dafe2768d7a1ea7405860
Randy <randy408@protonmail.com>
2018-07-27 14:39:29 +0300

    initial OSS-Fuzz integration
    add LibFuzzer fuzz target, dictionary and fuzzing options file

---
+++ tests/spng_read_fuzzer.cc
@@ -0,0 +1,24 @@
+#include "../spng.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    struct spng_ctx *ctx = spng_ctx_new();
+    if(ctx == NULL) return 0;
+
+    if(spng_set_png_buffer(ctx, data, size)) return 0;
+
+    spng_set_image_limits(ctx, 200000, 200000);
+
+    size_t out_size;
+    if(spng_decoded_image_size(ctx, SPNG_FMT_RGBA8, &out_size)) return 0;
+
+    unsigned char *out = (unsigned char*)malloc(out_size);
+    if(out == NULL) return 0;
+
+    if(spng_decode_image(ctx, SPNG_FMT_RGBA8, out, out_size, 0)) return 0;
+
+    spng_ctx_free(ctx);
+    free(out);
+
+    return 0;
+}
