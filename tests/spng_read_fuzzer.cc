10684d1632303cbd0c5147cd60d06c70d1973374
Randy <randy408@protonmail.com>
2019-01-08 15:03:32 +0200

    fix fuzz target error handling

--- tests/spng_read_fuzzer.cc
+++ tests/spng_read_fuzzer.cc
@@ -2,23 +2,26 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
+    unsigned char *out = NULL;
+
     spng_ctx *ctx = spng_ctx_new(0);
     if(ctx == NULL) return 0;
 
-    if(spng_set_png_buffer(ctx, (void*)data, size)) return 0;
+    if(spng_set_png_buffer(ctx, (void*)data, size)) goto err;
 
     spng_set_image_limits(ctx, 200000, 200000);
 
     size_t out_size;
-    if(spng_decoded_image_size(ctx, SPNG_FMT_RGBA8, &out_size)) return 0;
+    if(spng_decoded_image_size(ctx, SPNG_FMT_RGBA8, &out_size)) goto err;
 
-    unsigned char *out = (unsigned char*)malloc(out_size);
-    if(out == NULL) return 0;
+    out = (unsigned char*)malloc(out_size);
+    if(out == NULL) goto err;
 
-    if(spng_decode_image(ctx, out, out_size, SPNG_FMT_RGBA8, 0)) return 0;
+    if(spng_decode_image(ctx, out, out_size, SPNG_FMT_RGBA8, 0)) goto err;
 
+err:
     spng_ctx_free(ctx);
-    free(out);
+    if(out != NULL) free(out);
 
     return 0;
 }
