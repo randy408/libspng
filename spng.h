150d8d51c323383d44d34615180b9bf2b04e1f86
Randy <randy408@protonmail.com>
2019-07-17 11:03:36 +0200

    build: export symbols on windows

--- spng.h
+++ spng.h
@@ -6,6 +6,16 @@
 extern "C" {
 #endif
 
+#if defined(_MSC_VER) || defined(__MINGW32__) || defined(__CYGWIN__)
+    #ifndef SPNG_STATIC
+        #define SPNG_API __declspec(dllexport)
+    #else
+        #define SPNG_API __declspec(dllimport)
+    #endif
+#else
+    #define SPNG_API
+#endif
+
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -306,68 +316,68 @@ typedef struct spng_ctx spng_ctx;
 
 typedef int spng_read_fn(spng_ctx *ctx, void *user, void *data, size_t n);
 
-spng_ctx *spng_ctx_new(int flags);
-spng_ctx *spng_ctx_new2(struct spng_alloc *alloc, int flags);
-void spng_ctx_free(spng_ctx *ctx);
+SPNG_API spng_ctx *spng_ctx_new(int flags);
+SPNG_API spng_ctx *spng_ctx_new2(struct spng_alloc *alloc, int flags);
+SPNG_API void spng_ctx_free(spng_ctx *ctx);
 
-int spng_set_png_buffer(spng_ctx *ctx, void *buf, size_t size);
-int spng_set_png_stream(spng_ctx *ctx, spng_read_fn *read_fn, void *user);
+SPNG_API int spng_set_png_buffer(spng_ctx *ctx, void *buf, size_t size);
+SPNG_API int spng_set_png_stream(spng_ctx *ctx, spng_read_fn *read_fn, void *user);
 
-int spng_set_image_limits(spng_ctx *ctx, uint32_t width, uint32_t height);
-int spng_get_image_limits(spng_ctx *ctx, uint32_t *width, uint32_t *height);
+SPNG_API int spng_set_image_limits(spng_ctx *ctx, uint32_t width, uint32_t height);
+SPNG_API int spng_get_image_limits(spng_ctx *ctx, uint32_t *width, uint32_t *height);
 
 #if defined(SPNG_UNTESTED)
-int spng_set_chunk_limits(spng_ctx *ctx, size_t chunk_size, size_t cache_size);
-int spng_get_chunk_limits(spng_ctx *ctx, size_t *chunk_size, size_t *cache_size);
+SPNG_API int spng_set_chunk_limits(spng_ctx *ctx, size_t chunk_size, size_t cache_size);
+SPNG_API int spng_get_chunk_limits(spng_ctx *ctx, size_t *chunk_size, size_t *cache_size);
 #endif
 
-int spng_set_crc_action(spng_ctx *ctx, int critical, int ancillary);
+SPNG_API int spng_set_crc_action(spng_ctx *ctx, int critical, int ancillary);
 
-int spng_decoded_image_size(spng_ctx *ctx, int fmt, size_t *out);
+SPNG_API int spng_decoded_image_size(spng_ctx *ctx, int fmt, size_t *out);
 
-int spng_decode_image(spng_ctx *ctx, void *out, size_t out_size, int fmt, int flags);
+SPNG_API int spng_decode_image(spng_ctx *ctx, void *out, size_t out_size, int fmt, int flags);
 
-int spng_get_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
-int spng_get_plte(spng_ctx *ctx, struct spng_plte *plte);
-int spng_get_trns(spng_ctx *ctx, struct spng_trns *trns);
-int spng_get_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
-int spng_get_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
-int spng_get_gama(spng_ctx *ctx, double *gamma);
-int spng_get_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
-int spng_get_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
-int spng_get_srgb(spng_ctx *ctx, uint8_t *rendering_intent);
-int spng_get_text(spng_ctx *ctx, struct spng_text *text, uint32_t *n_text);
-int spng_get_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
-int spng_get_hist(spng_ctx *ctx, struct spng_hist *hist);
-int spng_get_phys(spng_ctx *ctx, struct spng_phys *phys);
-int spng_get_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t *n_splt);
-int spng_get_time(spng_ctx *ctx, struct spng_time *time);
+SPNG_API int spng_get_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
+SPNG_API int spng_get_plte(spng_ctx *ctx, struct spng_plte *plte);
+SPNG_API int spng_get_trns(spng_ctx *ctx, struct spng_trns *trns);
+SPNG_API int spng_get_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
+SPNG_API int spng_get_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
+SPNG_API int spng_get_gama(spng_ctx *ctx, double *gamma);
+SPNG_API int spng_get_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
+SPNG_API int spng_get_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
+SPNG_API int spng_get_srgb(spng_ctx *ctx, uint8_t *rendering_intent);
+SPNG_API int spng_get_text(spng_ctx *ctx, struct spng_text *text, uint32_t *n_text);
+SPNG_API int spng_get_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
+SPNG_API int spng_get_hist(spng_ctx *ctx, struct spng_hist *hist);
+SPNG_API int spng_get_phys(spng_ctx *ctx, struct spng_phys *phys);
+SPNG_API int spng_get_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t *n_splt);
+SPNG_API int spng_get_time(spng_ctx *ctx, struct spng_time *time);
 
-int spng_get_offs(spng_ctx *ctx, struct spng_offs *offs);
-int spng_get_exif(spng_ctx *ctx, struct spng_exif *exif);
+SPNG_API int spng_get_offs(spng_ctx *ctx, struct spng_offs *offs);
+SPNG_API int spng_get_exif(spng_ctx *ctx, struct spng_exif *exif);
 
 
-int spng_set_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
-int spng_set_plte(spng_ctx *ctx, struct spng_plte *plte);
-int spng_set_trns(spng_ctx *ctx, struct spng_trns *trns);
-int spng_set_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
-int spng_set_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
-int spng_set_gama(spng_ctx *ctx, double gamma);
-int spng_set_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
-int spng_set_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
-int spng_set_srgb(spng_ctx *ctx, uint8_t rendering_intent);
-int spng_set_text(spng_ctx *ctx, struct spng_text *text, uint32_t n_text);
-int spng_set_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
-int spng_set_hist(spng_ctx *ctx, struct spng_hist *hist);
-int spng_set_phys(spng_ctx *ctx, struct spng_phys *phys);
-int spng_set_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t n_splt);
-int spng_set_time(spng_ctx *ctx, struct spng_time *time);
+SPNG_API int spng_set_ihdr(spng_ctx *ctx, struct spng_ihdr *ihdr);
+SPNG_API int spng_set_plte(spng_ctx *ctx, struct spng_plte *plte);
+SPNG_API int spng_set_trns(spng_ctx *ctx, struct spng_trns *trns);
+SPNG_API int spng_set_chrm(spng_ctx *ctx, struct spng_chrm *chrm);
+SPNG_API int spng_set_chrm_int(spng_ctx *ctx, struct spng_chrm_int *chrm_int);
+SPNG_API int spng_set_gama(spng_ctx *ctx, double gamma);
+SPNG_API int spng_set_iccp(spng_ctx *ctx, struct spng_iccp *iccp);
+SPNG_API int spng_set_sbit(spng_ctx *ctx, struct spng_sbit *sbit);
+SPNG_API int spng_set_srgb(spng_ctx *ctx, uint8_t rendering_intent);
+SPNG_API int spng_set_text(spng_ctx *ctx, struct spng_text *text, uint32_t n_text);
+SPNG_API int spng_set_bkgd(spng_ctx *ctx, struct spng_bkgd *bkgd);
+SPNG_API int spng_set_hist(spng_ctx *ctx, struct spng_hist *hist);
+SPNG_API int spng_set_phys(spng_ctx *ctx, struct spng_phys *phys);
+SPNG_API int spng_set_splt(spng_ctx *ctx, struct spng_splt *splt, uint32_t n_splt);
+SPNG_API int spng_set_time(spng_ctx *ctx, struct spng_time *time);
 
-int spng_set_offs(spng_ctx *ctx, struct spng_offs *offs);
-int spng_set_exif(spng_ctx *ctx, struct spng_exif *exif);
+SPNG_API int spng_set_offs(spng_ctx *ctx, struct spng_offs *offs);
+SPNG_API int spng_set_exif(spng_ctx *ctx, struct spng_exif *exif);
 
-const char *spng_strerror(int err);
-const char *spng_version_string(void);
+SPNG_API const char *spng_strerror(int err);
+SPNG_API const char *spng_version_string(void);
 
 #ifdef __cplusplus
 }
