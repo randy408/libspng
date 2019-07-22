cba3f1c1a704609f47090b46858b08732ad5ab6d
Randy <randy408@protonmail.com>
2019-07-22 17:24:36 +0200

    fuzz: randomize format, flags, etc

--- tests/spng_read_fuzzer.cc
+++ tests/spng_read_fuzzer.cc
@@ -1,15 +1,52 @@
 #define SPNG_UNTESTED
 #include "../spng.h"
 
+#include <string.h>
+
+struct buf_state
+{
+    const uint8_t *data;
+    size_t bytes_left;
+};
+
+static int buffer_read_fn(spng_ctx *ctx, void *user, void *dest, size_t length)
+{
+    struct buf_state *state = (struct buf_state*)user;
+
+    if(length > state->bytes_left) return SPNG_IO_EOF;
+
+    memcpy(dest, state->data, length);
+
+    state->bytes_left -= length;
+    state->data += length;
+
+    return 0;
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
+    if(size < 2) return 0;
+
+    int flags = data[0];
+    int fmt = data[1] & 0x7F;
+    int stream = data[1] >> 7;
+
+    data+=2; size-=2;
+
+    int ret;
     unsigned char *out = NULL;
-    int flags;
 
     spng_ctx *ctx = spng_ctx_new(0);
     if(ctx == NULL) return 0;
 
-    if(spng_set_png_buffer(ctx, (void*)data, size)) goto err;
+    struct buf_state state;
+    state.data = data;
+    state.bytes_left = size;
+
+    if(stream) ret = spng_set_png_stream(ctx, buffer_read_fn, &state);
+    else ret = spng_set_png_buffer(ctx, (void*)data, size);
+    
+    if(ret) goto err;
 
     spng_set_image_limits(ctx, 200000, 200000);
     
@@ -18,15 +55,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     spng_set_crc_action(ctx, SPNG_CRC_USE, SPNG_CRC_USE);
 
     size_t out_size;
-    if(spng_decoded_image_size(ctx, SPNG_FMT_RGBA8, &out_size)) goto err;
+    if(spng_decoded_image_size(ctx, fmt, &out_size)) goto err;
 
     if(out_size > 80000000) goto err;
 
     out = (unsigned char*)malloc(out_size);
     if(out == NULL) goto err;
 
-    flags = SPNG_DECODE_USE_TRNS | SPNG_DECODE_USE_GAMA | SPNG_DECODE_USE_SBIT;
-    if(spng_decode_image(ctx, out, out_size, SPNG_FMT_RGBA8, flags)) goto err;
+    if(spng_decode_image(ctx, out, out_size, fmt, flags)) goto err;
 
 err:
     spng_ctx_free(ctx);
