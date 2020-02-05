5ef25ac5343e65699922266de6c537267bb6a653
Randy <randy408@protonmail.com>
2020-02-05 18:58:40 +0100

    ossfuzz: cleanup

--- tests/ossfuzz.sh
+++ tests/ossfuzz.sh
@@ -47,5 +47,4 @@ $CXX $CXXFLAGS -std=c++11 -I. \
 find $SRC/libspng/tests/images -name "*.png" | \
      xargs zip $OUT/spng_read_fuzzer_seed_corpus.zip
 
-cp $SRC/libspng/tests/spng.dict \
-   $SRC/libspng/tests/spng_read_fuzzer.options $OUT/
+cp $SRC/libspng/tests/spng_read_fuzzer.dict
