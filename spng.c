d9f386ba3a20fb757501157f44d68da6ca13a624
Randy <randy408@protonmail.com>
2020-06-01 11:10:56 +0200

    decode: error out on output rows larger than UINT32_MAX
    It's not worth switching to 64-bit arithmetic for all the offset calculations,
    4GB ought to be enough for anyone.

--- spng.c
+++ spng.c
@@ -2853,6 +2853,8 @@ int spng_decode_image(spng_ctx *ctx, unsigned char *out, size_t len, int fmt, in
     {
         if(fmt & (SPNG_FMT_PNG | SPNG_FMT_RAW)) sub[i].out_width = sub[i].scanline_width - 1;
         else sub[i].out_width = (size_t)sub[i].width * pixel_size;
+
+        if(sub[i].out_width > UINT32_MAX) return decode_err(ctx, SPNG_EOVERFLOW);
     }
 
     /* Read the first filter byte, offsetting all reads by 1 byte.
