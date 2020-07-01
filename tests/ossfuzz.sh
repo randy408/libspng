fad8464dbc110daae7f42cd1489e3c5d48112fd4
Randy <randy408@protonmail.com>
2020-07-01 05:46:46 +0200

    Fix MSan build on OSS-Fuzz (#104)

--- tests/ossfuzz.sh
+++ tests/ossfuzz.sh
@@ -6,20 +6,23 @@
 mkdir $SRC/libspng/zlib/build $SRC/libspng/build
 cd $SRC/libspng/zlib/build
 cmake ..
-make -j$(nproc) install
-ldconfig
-
-cd $SRC/libspng/build
-cmake -DSPNG_STATIC=ON -DCMAKE_C_COMPILER_WORKS=1 ..
 make -j$(nproc)
-cd ..
 
-$CXX $CXXFLAGS -std=c++11 -I. \
+$CC $CFLAGS -c -I$SRC/libspng/zlib/build -I$SRC/libspng/zlib \
+    $SRC/libspng/spng/spng.c \
+    -o $SRC/libspng/build/libspng.o \
+
+cp $SRC/libspng/zlib/build/libz.a $SRC/libspng/build/libz.a
+cd $SRC/libspng/build
+ar x libz.a
+ar rcs libspng_static.a *.o
+
+$CXX $CXXFLAGS -std=c++11 \
     $SRC/libspng/tests/spng_read_fuzzer.cc \
     -o $OUT/spng_read_fuzzer \
     $LIB_FUZZING_ENGINE $SRC/libspng/build/libspng_static.a $SRC/libspng/zlib/build/libz.a
 
-$CXX $CXXFLAGS -std=c++11 -I. \
+$CXX $CXXFLAGS -std=c++11 -I$SRC/libspng/zlib/build -I$SRC/libspng/zlib \
     $SRC/libspng/tests/spng_read_fuzzer.cc \
     -o $OUT/spng_read_fuzzer_structure_aware \
     -include $SRC/fuzzer-test-suite/libpng-1.2.56/png_mutator.h \
