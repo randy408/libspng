ffa7f41b13e2529bffbc5a366f0a315fcdd36b3c
Randy <randy408@protonmail.com>
2020-08-14 13:55:56 +0200

    dont check tEXt, zTXt chunk text fields in default mode

--- spng/spng.c
+++ spng/spng.c
@@ -2195,7 +2195,7 @@ static int read_non_idat_chunks(spng_ctx *ctx)
                     language_tag_offset = keyword_len;
                     translated_keyword_offset = keyword_len;
 
-                    if(check_png_text(text->text, text->text_length)) return SPNG_ETEXT;
+                    if(ctx->strict && check_png_text(text->text, text->text_length)) return SPNG_ETEXT;
                 }
 
                 text->language_tag = text->keyword + language_tag_offset;
