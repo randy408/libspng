d5569bed8a9a40e44a68dac6c96b5bf9a6b352d2
Randy <randy408@protonmail.com>
2020-09-20 00:49:58 +0200

    ossfuzz: build with SPNGT_HAVE_FMEMOPEN

--- tests/ossfuzz.sh
+++ tests/ossfuzz.sh
@@ -19,11 +19,13 @@ ar rcs libspng_static.a *.o
 
 $CXX $CXXFLAGS -std=c++11 \
     $SRC/libspng/tests/spng_read_fuzzer.c \
+    -DSPNGT_HAVE_FMEMOPEN=1 \
     -o $OUT/spng_read_fuzzer \
     $LIB_FUZZING_ENGINE $SRC/libspng/build/libspng_static.a $SRC/zlib/build/libz.a
 
 $CXX $CXXFLAGS -std=c++11 -I$SRC/zlib/build -I$SRC/zlib \
     $SRC/libspng/tests/spng_read_fuzzer.c \
+    -DSPNGT_HAVE_FMEMOPEN=1 \
     -o $OUT/spng_read_fuzzer_structure_aware \
     -include $SRC/fuzzer-test-suite/libpng-1.2.56/png_mutator.h \
     -D PNG_MUTATOR_DEFINE_LIBFUZZER_CUSTOM_MUTATOR \
